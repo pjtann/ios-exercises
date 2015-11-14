743b43d47a3c5317b851cf5f5ccc365a555308cb
PT <PT@PTs-MacBook-Pro.local>
2015-11-14 04:13:14 -0700

    Assignment Foundation 10 Equality Completed

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,45 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    // PJT - can't use == because the variables don't occupy the same memory space since they were never assigned to each other, both set independently equal to the cereal name. So must use 'isEqual' for equality; or BOOL statement as below
+    if ([string1 isEqualToString:string2])
+        return YES;
+    else
+        return NO;
+
+    // OR this BOOL test also works
+//    BOOL areTheyEqual = string1 == string2;
+//    return YES;
+    
+    // PJT - original return
+//    return (string1 == string2);
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    // PJT - have to use iEqual again since the values were never assigned to each other and therefore occupy different memory spaces
+    if ([number1 isEqual:number2])
+        return YES;
+    else
+        return NO;
+    // PJT - original
+//    return (number1 == number2);
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    
+    // PJT - using BOOL statements
+//    BOOL testGreaterThan = integer1 > integer2;
+//    return testGreaterThan;
+    
+    // PJT - or using if statements
+    if (integer1 > integer2)
+        return YES;
+    else
+        return NO;
+    // PJT - original statement
+    //return NO;
 }
 
 @end
