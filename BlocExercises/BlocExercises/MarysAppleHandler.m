64a3a1bcb5c439b1ffff8c064ec97e52bbfe8807
PT <PT@PTs-MacBook-Pro.local>
2015-11-16 09:10:36 -0700

    Assignment Foundation 11 If/Else Completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,7 +14,17 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
-    
+    if (dollars <= 4)
+        itemToReturn = @"get out of my store";
+    if (dollars == 5)
+        itemToReturn = @"have some gum";
+    if (dollars == 6)
+        itemToReturn = @"have an apple";
+    if (dollars >= 1000)
+        itemToReturn = @"have an Apple computer";
+    if (dollars == 1000000000)
+        itemToReturn = @"have The Big Apple";
+ 
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
@@ -23,10 +33,16 @@
     /* WORK HERE */
 
     NSUInteger cost = 24;
+    cost = (self.getsDiscount) ? cost *= .75 : cost;
+    
+    /*
+    // original statements
+    NSUInteger cost = 24;
     
     if (self.getsDiscount) {
         cost *= .75;
     }
+    */
     
     return cost;
 }
