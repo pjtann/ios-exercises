d6e50c255b249a52d2ea4293bbab18a184d6dde7
PT <PT@PTs-MacBook-Pro.local>
2015-11-17 19:52:58 -0700

    Assignment Foundation 12 Loops Completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,31 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSInteger firstNumber = 0;
+    NSInteger secondNumber = 0;
+    
+    if (number < otherNumber){
+        firstNumber = number;
+        secondNumber = otherNumber;
+    }else{
+        firstNumber = otherNumber;
+        secondNumber = number;
+    
+    }
+    
+    NSMutableString *myString = [NSMutableString stringWithFormat:@""];
+    NSString *myIntString = NULL;
+    
+    for (NSInteger n = firstNumber; n <= secondNumber; n++){
+        myIntString = [NSString stringWithFormat:@"%ld",(long)n];
+        
+        [myString appendString:myIntString];
+        
+        
+    }
+    // original statement
+    //return @"";
+    return  myString;
 }
 
 @end
