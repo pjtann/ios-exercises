deb69f754d5b97de2dad84fe5b85b07147d8c8df
PT <PT@PTs-MacBook-Pro.local>
2015-11-19 19:54:25 -0700

    Assignment Foundation 13 Arrays Completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,44 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    NSArray *myStarTrekArray = [characterString componentsSeparatedByString:@";"];
+        [myStarTrekArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"Test block.");
+    }];
+    //return @[];
+    return myStarTrekArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *myStarTrekString = [characterArray componentsJoinedByString:@";"];
+
+    //return @"";
+    return myStarTrekString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *mySortedArray = [characterArray mutableCopy];
+    NSSortDescriptor *mySortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [mySortedArray sortUsingDescriptors:@[mySortDescriptor]];
+    
+    //return @[];
+    return mySortedArray;
+    
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    NSMutableArray *myWorfArray = [characterArray mutableCopy];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+
+    [myWorfArray filterUsingPredicate:containsWorf];
+        if (myWorfArray.count > 0){
+            return YES;
+        }
     return NO;
+    //return YES;
 }
 
 @end
