5b2240627d4de05ee1c53131164092ca7be4aa0e
PT <PT@PTs-MacBook-Pro.local>
2015-11-21 12:55:04 -0700

    Assignment Foundation 15 Simple Data Types Completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,53 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    int test = [number intValue];
+    int twiceNumber = (2 * test);
+    NSNumber *bigNumber = [NSNumber numberWithInt:twiceNumber];
+  
+    //return @0;
+    return bigNumber;
+    
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSInteger firstNumber = 0;
+    NSInteger secondNumber = 0;
+    NSMutableArray *myArray = [[NSMutableArray alloc]init];
+    
+    if (number < otherNumber){
+        firstNumber = number;
+        secondNumber = otherNumber;
+    }else{
+        firstNumber = otherNumber;
+        secondNumber= number;
+    }
+    for (long n=firstNumber; n >=firstNumber && n<=secondNumber; n++) {
+        [myArray addObject:[NSNumber numberWithInteger:n]];
+        for (int arrayCounter=0; arrayCounter<myArray.count; arrayCounter++) {
+            NSLog(@"myArray..: %@", myArray[arrayCounter]);
+        }
+    }    
+    //return @[];
+    return myArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSNumber *lowestNumber;
+    //NSInteger lowInt;
+    for (int n=0; n<arrayOfNumbers.count; n++) {
+        NSNumber *newNumber = arrayOfNumbers[n];
+        if (n==0) {
+            lowestNumber = newNumber;
+        }
+        if ([newNumber intValue] < [lowestNumber intValue]) {
+            lowestNumber = newNumber;
+        }
+    }
+    //return 0;
+    return [lowestNumber intValue];
 }
 
 @end
