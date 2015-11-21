e5facd9cb8af45862d442663dca3ad20e7147e37
PT <PT@PTs-MacBook-Pro.local>
2015-11-20 20:25:51 -0700

    Assignment Foundation 14 Dictionaries Completed

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,53 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    __block NSString *favDrink = nil;
+    
+    [characterDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        if ([key  isEqual: @"favorite drink"])
+        {
+            NSLog(@"Favorite Drink...key: %@, and Object: %@", key, obj);
+            //NSString *favDrink = obj;
+            favDrink = obj;
+            //return favDrink;
+        }
+    }];
+    //return @"";
+    return favDrink;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    __block NSMutableArray *favDrinkArray = [[NSMutableArray alloc]init]; // this allows add objects
+    //__block NSMutableArray *favDrinkArray = nil; // this won't allow me to add objects
+
+        [charactersArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSDictionary *myDictionary = obj;
+            [myDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+                if ([key isEqual: @"favorite drink"]) {
+                    NSLog(@"Favorite Drink...key=%@, and object=%@", key, obj);
+                    
+                    //NSString *newString = @"hello";
+                    //[favDrinkArray addObject:newString];
+                    [favDrinkArray addObject:obj];
+                 
+                }
+            }];
+        }];
+    //}
+    
+    //return @[];
+    return favDrinkArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *myChangingDictionary = [characterDictionary mutableCopy];
+    [myChangingDictionary setObject:@"This is Picards quote." forKey:@"quote"];
+    
+    
+    //return @{};
+    return myChangingDictionary;
 }
 
 @end
